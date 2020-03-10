9f3f8e12902ebbc075d22b9ed1da4642bbb48150
Matthew Bolton <matthewwbolton@gmail.com>
2020-03-10 19:03:00 -0400

    completed search functionality and updated followers component to have a dynamic url

--- github-user-card/src/components/GithubFollowersCard.jsx
+++ github-user-card/src/components/GithubFollowersCard.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import Axios from 'axios';
 import {
-    Card, CardImg, CardText, CardBody, CardLink,
-    CardTitle, CardSubtitle
+    Card, CardBody, CardLink,
+    CardTitle
   } from 'reactstrap';
   import styled from 'styled-components';
 
-  const NewImg = styled.img`
-    width: 40%;
-  `;
+//   const NewImg = styled.img`
+//     width: 40%;
+//   `;
 
   const NewDiv = styled.div`
     display: flex;
@@ -24,25 +24,36 @@ export default class GithubFollowersCard extends Component {
     constructor() {
         super();
         this.state = {
-            followers: [],
+            followers: [] 
+            
         }
     }
 
     componentDidMount(){
+        console.log(`the followers component did mount`, this.props.user)
         Axios
             .get(`https://api.github.com/users/${this.props.user.login}/followers`)
             .then(res => {
-                console.log(res);
+                console.log(res.data, this.props.user);
                 this.setState({
                     followers: res.data
                 })
             })
             .catch(err => console.log(err.message));
     }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.followers !== this.state.followers){
+            console.log('component did update');
+        }
+    }
+
+   
     
     
     
     render() {
+        console.log(this.props.user);
         return (
             <NewDiv>
                 {this.state.followers.map(follower => (
@@ -50,7 +61,7 @@ export default class GithubFollowersCard extends Component {
                 <CardBody>
                   <CardTitle>Github Handle: {follower.login}</CardTitle>
                 </CardBody>
-                <NewImg width="100%" src={follower.avatar_url} alt={follower.login} />
+                <img width="100%" src={follower.avatar_url} alt={follower.login} />
                 <CardBody>
                   <CardLink href={follower.url}>Profile Link</CardLink>
                 </CardBody>
--- github-user-card/src/components/GithubPage.jsx
+++ github-user-card/src/components/GithubPage.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import GithubUserCard from './GithubUserCard';
 import axios from 'axios';
 import GithubFollowersCard from './GithubFollowersCard';
+import GithubSearchForm from './GitHubSearchForm';
 
 
 export default class GithubPage extends Component {
@@ -10,21 +11,59 @@ export default class GithubPage extends Component {
         super();
         this.state = {
             user: [],
+            userHandle: '',
         }
     }
 
     componentDidMount() {
         axios
-            .get('https://api.github.com/users/matthewwbolton')
+            .get(`https://api.github.com/users/matthewwbolton`)
             .then(res => {
                 console.log(res.data);
                 this.setState({
                     user: res.data
+                
                 })
                 
             })
             .catch(err => console.log(err.message));
     }
+
+    
+    handleChanges = e => {
+        this.setState({
+            userHandle: e.target.value
+        });
+    };
+    
+    
+    fetchUser = e => {
+        e.preventDefault();
+        axios
+            .get(`https://api.github.com/users/${this.state.userHandle}`)
+            .then(res => {
+                console.log(res);
+                this.setState({
+                    user: res.data
+                })
+            })
+            .catch(err => console.log(err.message))
+
+    }
+
+    // componentDidUpdate(prevProps, prevState) {
+    //     if (prevState.user !== this.state.user){
+    //         console.log(`new user in state`);
+    //         axios
+    //             .get(`https://api.github.com/users/${this.state.user.login}/followers}`)
+    //             .then(res => {
+    //                 console.log(res.data);
+    //                 this.setState){
+    //                     followers: res.data
+    //                 }
+    //             })
+    //     }
+    // }
 
    
 
@@ -32,9 +71,10 @@ export default class GithubPage extends Component {
    
    
     render() {
+        console.log(this.state.user);
         return (
             <div>
-             
+                <GithubSearchForm  fetchUser={this.fetchUser} handleChanges={this.handleChanges}/>
                 <GithubUserCard user={this.state.user}/>
                 <GithubFollowersCard user={this.state.user}/>
               
